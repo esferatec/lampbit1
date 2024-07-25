686f72806b1db3083d5313b7ebecba1e24c5e5c5
esferatec <136970939+esferatec@users.noreply.github.com>
2024-07-25 21:19:28 +0200

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -53,6 +53,13 @@ function showFourSingleColor (color1: number, color2: number, color3: number, co
     Strip3.showColor(color3)
     Strip4.showColor(color4)
 }
+input.onButtonPressed(Button.A, function () {
+    if (RTC_DS1307.getTime(RTC_DS1307.TimeType.HOUR) < 23) {
+        RTC_DS1307.setTime(RTC_DS1307.TimeType.HOUR, RTC_DS1307.getTime(RTC_DS1307.TimeType.HOUR) + 1)
+    } else {
+        RTC_DS1307.setTime(RTC_DS1307.TimeType.HOUR, 0)
+    }
+})
 function circleAllSingleColor (color: number) {
     Strip1.showColor(color)
     basic.pause(500)
@@ -72,7 +79,7 @@ function shiftAllRainbow () {
     Strip2.showRainbow(1, 360)
     Strip3.showRainbow(1, 360)
     Strip4.showRainbow(1, 360)
-    for (let index = 0; index <= Strip1.length(); index++) {
+    for (let index2 = 0; index2 <= Strip1.length(); index2++) {
         Strip1.shift(1)
         Strip1.show()
         Strip2.shift(1)
@@ -114,6 +121,13 @@ function showAllSingleColor (color: number) {
     Strip3.showColor(color)
     Strip4.showColor(color)
 }
+input.onButtonPressed(Button.B, function () {
+    if (RTC_DS1307.getTime(RTC_DS1307.TimeType.MINUTE) < 59) {
+        RTC_DS1307.setTime(RTC_DS1307.TimeType.MINUTE, RTC_DS1307.getTime(RTC_DS1307.TimeType.MINUTE) + 1)
+    } else {
+        RTC_DS1307.setTime(RTC_DS1307.TimeType.MINUTE, 0)
+    }
+})
 input.onGesture(Gesture.Shake, function () {
     modeNumber = randint(0, 10)
     colorNumber = randint(0, 8)
@@ -128,7 +142,7 @@ function shiftAllSingleColor (color: number) {
     Strip2.showColor(color)
     Strip3.showColor(color)
     Strip4.showColor(color)
-    for (let index = 0; index <= Strip1.length(); index++) {
+    for (let index3 = 0; index3 <= Strip1.length(); index3++) {
         Strip1.shift(1)
         Strip1.show()
         Strip2.shift(1)
@@ -140,6 +154,7 @@ function shiftAllSingleColor (color: number) {
         basic.pause(200)
     }
 }
+let time = 0
 let colorNumber4 = 0
 let colorNumber3 = 0
 let colorNumber2 = 0
@@ -160,8 +175,8 @@ Strip3.showColor(neopixel.colors(NeoPixelColors.Black))
 Strip4 = neopixel.create(DigitalPin.P16, 15, NeoPixelMode.RGB)
 Strip4.showColor(neopixel.colors(NeoPixelColors.Black))
 let Display = TM1637.create(
+DigitalPin.P2,
 DigitalPin.P1,
-DigitalPin.P9,
 7,
 4
 )
@@ -179,7 +194,19 @@ neopixel.colors(NeoPixelColors.Violet),
 neopixel.colors(NeoPixelColors.Purple),
 neopixel.colors(NeoPixelColors.White)
 ]
+RTC_DS1307.DateTime(
+2025,
+7,
+25,
+20,
+25,
+0
+)
 basic.forever(function () {
+    time = RTC_DS1307.getTime(RTC_DS1307.TimeType.HOUR) * 100
+    time = time + RTC_DS1307.getTime(RTC_DS1307.TimeType.MINUTE)
+    Display.showNumber(time)
+    Display.showDP(1, true)
     basic.showNumber(modeNumber)
     if (modeNumber == 0) {
         showAllSingleColor(colorNumber)
@@ -214,7 +241,4 @@ basic.forever(function () {
     if (modeNumber == 10) {
         circleAllRainbow()
     }
-})
-control.inBackground(function () {
-    Display.showNumber(input.temperature())
 })
